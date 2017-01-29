ae30f846668d0b92d218c73b5010f96d4b0b71c9
Ethan Choi <ethanchoi.812@gmail.com>
2017-01-29 15:18:43 +0800

    Added control to: 1. toggle between Pomodoro and break, 2. increase/decrease duration

--- app.js
+++ app.js
@@ -1,19 +1,50 @@
 	var pomo = document.getElementById("setPomo");
-	var pomoTime = pomo.innerHTML;
+	pomo.innerHTML = 25;
 
 	var breko = document.getElementById("setBreak");
-	var breakTime = breko.innerHTML;
+	breko.innerHTML = 05;
 
-	var display = document.getElementById("displayTimer");
+	var display = document.getElementById("displayTime");
+	var countDown;
+
+	var minusPomoBtn = document.getElementById("minusPomo");
+	var plusPomoBtn = document.getElementById("plusPomo");
+
+	var minusBreakBtn = document.getElementById("minusBreak");
+	var plusBreakBtn = document.getElementById("plusBreak");
+
+//click plus / minus to set time
+	minusPomoBtn.onclick = function(){
+		pomo.innerHTML = parseInt(pomo.innerHTML) - 1;
+	}
+
+	plusPomoBtn.onclick = function(){
+		pomo.innerHTML = parseInt(pomo.innerHTML) + 1;
+	}
+
+	minusBreakBtn.onclick = function(){
+		breko.innerHTML = parseInt(breko.innerHTML) - 1;
+	}
+
+	plusBreakBtn.onclick = function(){
+		breko.innerHTML = parseInt(breko.innerHTML) + 1;
+	}
+
+//click number/label to set countdown
+//click number to countdown
 
 var initializeClock = function(endtime){
 
-	var t = endtime*60;
-	var countDown = setInterval(function(){
-
-		t--;
+	var t = endtime*60 + 1; 
+	
+	countDown = setInterval(function(){
+		t-= 1;
 		var minutes = Math.floor(t/60);
 		var seconds = Math.floor(t%60);
+
+		if(minutes<10){
+			display.innerHTML = "0" + minutes + ":" + seconds;
+		} 
 
 		if(seconds<10){
 			display.innerHTML = minutes + ":" + "0" + seconds;
@@ -29,14 +60,17 @@ var initializeClock = function(endtime){
 	}, 1000);
 }
 
+//change between pomodoro and break
 function init(){
 	pomo.onclick = function(){
-		display.innerHTML = "";
+		var pomoTime = pomo.innerHTML;
+		clearInterval(countDown);
 		initializeClock(pomoTime);
 	}
 
 	breko.onclick = function(){
-		display.innerHTML = "";
+		var breakTime = breko.innerHTML;
+		clearInterval(countDown);
 		initializeClock(breakTime);
 	}
 	return false;
